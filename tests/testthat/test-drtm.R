be170160843fd0a937e9702ba6daad4d95eedf84
Merlin Unterfinger <info@munterfinger.ch>
2020-10-05 15:04:29 +0200

    [ci] Add tests for various options in model optimization.

--- tests/testthat/test-drtm.R
+++ tests/testthat/test-drtm.R
@@ -23,10 +23,19 @@ test_that("drtm works", {
   expect_is(m, "drtm")
   expect_equal(length(m), 9)
 
-  # Iterate model
-  m <- drt_iterate(m, 10)
+  # Iterate model with precalculation
+  i = 3
+  m_tt <- drt_iterate(m, i, precalculate = TRUE, annealing = TRUE)
+  m_tf <- drt_iterate(m, i, precalculate = TRUE, annealing = FALSE)
 
-  # Test iteration
-  expect_equal(m$i, 10)
+  # Iterate model without precalculation
+  m_ft <- drt_iterate(m, i, precalculate = FALSE, annealing = TRUE)
+  m_ff <- drt_iterate(m, i, precalculate = FALSE, annealing = FALSE)
+
+  # Test iterations
+  expect_equal(m_tt$i, i)
+  expect_equal(m_tf$i, i)
+  expect_equal(m_ft$i, i)
+  expect_equal(m_ff$i, i)
 
 })
